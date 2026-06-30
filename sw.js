dd982bce9c9cb3526a1550cbb88145274032aa88
SiroB-jpg <sirobattaglin@googlemail.com>
2026-06-30 16:15:36 +1000

    Update to v1.1.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'italian-shadowing-studio-v1-0-9';
+const CACHE_NAME = 'italian-shadowing-studio-v1-1-0';
 const ASSETS = ['./index.html','./app.js','./styles.css','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
